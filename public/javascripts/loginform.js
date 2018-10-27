546bc79f39e7bed1f359adc4f196f765fd308256
thomas <thomasgrz@outlook.com>
2018-10-27 16:47:08 -0400

    type: checkup

--- public/javascripts/loginform.js
+++ public/javascripts/loginform.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded",()=>{
     console.log("login.js is loaded up")
     let loginform = document.getElementById("login")
     loginform.addEventListener("submit",(event)=>{
-        // event.preventDefault()
+        event.preventDefault()
         })
 })
