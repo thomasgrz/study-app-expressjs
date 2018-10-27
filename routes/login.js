f30141432bef729eb01db3fd88f372c6f152946c
thomas <thomasgrz@outlook.com>
2018-10-27 11:38:17 -0400

    fixing app crash bug

--- routes/login.js
+++ routes/login.js
@@ -1,4 +1,5 @@
 var config = require('dotenv').config().parsed
+var { Client } = require("pg");
 var express = require('express')
 var router = express.Router();
 
@@ -9,10 +10,21 @@ router.get('/',function(req,res,next){
 router.post('/',function(req,res){
     // console.log(this)
     // res.send(this)
-    db.one('SELECT * FROM users WHERE name=$1',req.body.username)
-        .then((data)=>{
-            console.log("DATA: ",data)
-        })
-        .catch((error)=>console.error(error))
+    const client = new Client({
+        connectionString: process.env.DATABASE_URL,
+        ssl:true,
+      })
+      
+      client.connect().catch((err)=>console.error(err))
+      
+      client.query('SELECT * FROM users WHERE name=\'thomas\';',(err,res)=>{
+        if(err){
+          throw err;
+        }
+        for(let row of res.rows){
+          console.log(JSON.stringify(row))
+        }
+        client.end()
+      });
 })
 module.exports = router;
